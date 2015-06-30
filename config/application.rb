f945530ce541aad1d11351610e1a095f9cfdc326
roshan92 <r.sellvan@quamble.com.my>
2015-06-30 13:29:16 +0800

    fixing heroku ckeditor bug

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Ticketing
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+    config.time_zone = 'Kuala Lumpur'
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -23,6 +23,8 @@ module Ticketing
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
